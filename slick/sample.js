f7c28f853e6e1f57afe42fdc0cde97ddd4b45c7d
genfuan <tocojaws2410@gmail.com>
2019-01-17 15:50:57 +0900

    aaa

---
+++ slick/sample.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.single-item').slick({
+    accessibility: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+//	  variableWidth:true,
+    dots: true,
+    fade: true,
+  });
+});
